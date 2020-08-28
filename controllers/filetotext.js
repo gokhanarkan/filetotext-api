f3f7759c270e2078a4db77b8c9b9db0ab8bb1efe
Gokhan <gokhanarkan@gmail.com>
2020-08-28 12:25:25 +0300

    tesseract added as a default reading option atm

--- controllers/filetotext.js
+++ controllers/filetotext.js
@@ -1,5 +1,6 @@
 const ErrorResponse = require("../utils/errorResponse");
 const AWS = require("aws-sdk");
+const { createWorker } = require("tesseract.js");
 
 exports.convertFile = async (req, res, next) => {
   const file = req.files.file;
@@ -8,9 +9,39 @@ exports.convertFile = async (req, res, next) => {
     return next(new ErrorResponse("File is not here", 400));
   }
 
+  // Currently accepting only jpeg and png
+  const acceptedTypes = ["image/jpeg", "image/png"];
+
+  if (!acceptedTypes.includes(file.mimetype)) {
+    return next(new ErrorResponse("File type is not supported.", 400));
+  }
+
+  const worker = createWorker();
+
+  await worker.load();
+  await worker.loadLanguage("eng");
+  await worker.initialize("eng");
+  const {
+    data: { text },
+  } = await worker.recognize(file.data);
+  await worker.terminate();
+
+  res.status(200).json({
+    success: true,
+    data: text,
+  });
+
   // TODO
   // Validate size
   // Valida mimetype
+};
+
+exports.convertFileWithAWS = async (req, res, next) => {
+  const file = req.files.file;
+
+  if (!file) {
+    return next(new ErrorResponse("File is not here", 400));
+  }
 
   try {
     const textract = new AWS.Textract({
@@ -29,16 +60,16 @@ exports.convertFile = async (req, res, next) => {
       if (err) {
         return next(new ErrorResponse("Error with AWS", 500));
       }
-      let responseData = ""
-      data.Blocks.forEach(item => {
-        if (item.BlockType === 'WORD') {
-          responseData += item.Text + " "
+      let responseData = "";
+      data.Blocks.forEach((item) => {
+        if (item.BlockType === "WORD") {
+          responseData += item.Text + " ";
         }
-      })
+      });
       res.status(200).json({
         success: true,
-        data: responseData
-      })
+        data: responseData,
+      });
     });
   } catch (err) {
     return next(new ErrorResponse("Error with AWS", 500));
